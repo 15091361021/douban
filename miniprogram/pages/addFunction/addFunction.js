4e2025a3065050435d737537dd92589d1892dab4
shipei <903812316@qq.com>
2020-03-29 22:51:49 +0800

    加入tabBar

--- miniprogram/pages/addFunction/addFunction.js
+++ miniprogram/pages/addFunction/addFunction.js
@@ -6,6 +6,7 @@ exports.main = (event, context) => {
   console.log(context)
   return {
     sum: event.a + event.b
+    sum1：event.a+event.b
   }
 }`
 
@@ -30,13 +31,37 @@ Page({
       }
     })
   },
+  testFunction1(){
+    wx.cloud.callFunction({
+      name:'sum1',
+      data:{
+        a:10,
+        b:50
+      },
+      success:res=>{
+        wx.showToast({
+          title: '调用成功1',
+        })
+        this.setData({
+          result: JSON.stringify(res.result)
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '调用失败',
+        })
+        console.error('[云函数] [sum] 调用失败：', err)
+      }
+    })
+  },
 
   testFunction() {
     wx.cloud.callFunction({
       name: 'sum',
       data: {
-        a: 1,
-        b: 2
+        a: 10,
+        b: 22
       },
       success: res => {
         wx.showToast({
