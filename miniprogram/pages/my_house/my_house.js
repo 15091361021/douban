0cce5168a6f7c8fb5733c117e355388bfdef832f
15091361021 <903812316@qq.com>
2020-04-11 17:16:38 +0800

    test database

--- miniprogram/pages/my_house/my_house.js
+++ miniprogram/pages/my_house/my_house.js
@@ -10,22 +10,44 @@ Page({
   sex: "男"
  },
  formSubmit: function (e) {
-  console.log('form发生了submit事件，携带数据为：', e.detail.value);
-  let { phone, pwd, isPub, sex } = e.detail.value;
-  if (!phone || !pwd) {
-   this.setData({
-    warn: "手机号或密码为空！",
-    isSubmit: true
-   })
-   return;
-  }
-  this.setData({
-    warn: "",
-    isSubmit: true,
-    phone,
-    pwd,
-    isPub,
-    sex
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    let { phone, pwd, isPub, sex } = e.detail.value;
+    if (!phone || !pwd) {
+    this.setData({
+      warn: "手机号或密码为空！",
+      isSubmit: true
+    })
+    return;
+    }
+    this.setData({
+      warn: "",
+      isSubmit: true,
+      phone,
+      pwd,
+      isPub,
+      sex
+    })
+    const db = wx.cloud.database()
+    db.collection('test').add({
+      data: this.data,
+      success: res => {
+        // 在返回结果中会包含新创建的记录的 _id
+        this.setData({
+          counterId: res._id,
+          count: 1
+        })
+        wx.showToast({
+          title: '新增记录成功',
+        })
+        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '新增记录失败'
+        })
+        console.error('[数据库] [新增记录] 失败：', err)
+      }
     })
   },
   formReset: function () {
